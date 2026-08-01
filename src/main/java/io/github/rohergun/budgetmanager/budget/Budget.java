eb04de9b862601fbfe942218e783845e29c52a3b
RohatErgun <rohatergun06@gmail.com>
2026-08-01 10:02:32 +0200

    feat: add Budget class jpa relationships

--- src/main/java/io/github/rohergun/budgetmanager/budget/Budget.java
+++ src/main/java/io/github/rohergun/budgetmanager/budget/Budget.java
@@ -1,13 +1,15 @@
 package io.github.rohergun.budgetmanager.budget;
 
+import io.github.rohergun.budgetmanager.category.Category;
 import io.github.rohergun.budgetmanager.model.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import io.github.rohergun.budgetmanager.user.AppUser;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.math.BigDecimal;
 
@@ -20,4 +22,13 @@ public class Budget extends BaseEntity {
 
     @Column(name = "monthly_limit", nullable = false, precision = 12, scale = 2)
     private BigDecimal monthlyLimit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = true)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    private Category category;
 }
