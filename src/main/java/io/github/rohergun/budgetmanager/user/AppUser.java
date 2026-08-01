bb8395903a27e5c3c815571a1f196a551d0af23d
RohatErgun <rohatergun06@gmail.com>
2026-08-01 09:59:37 +0200

    feat: add AppUser jpa relationships

--- src/main/java/io/github/rohergun/budgetmanager/user/AppUser.java
+++ src/main/java/io/github/rohergun/budgetmanager/user/AppUser.java
@@ -1,9 +1,15 @@
 package io.github.rohergun.budgetmanager.user;
 
+import io.github.rohergun.budgetmanager.budget.Budget;
 import io.github.rohergun.budgetmanager.model.BaseEntity;
+import io.github.rohergun.budgetmanager.transaction.Transaction;
+import io.github.rohergun.budgetmanager.financialgoal.FinancialGoal;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -29,4 +35,16 @@ public class AppUser extends BaseEntity {
     @Column(nullable = false, name = "password_hash")
     @NotBlank
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Budget> budgets = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Transaction> transactions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<FinancialGoal> financialGoals = new ArrayList<>();
 }
