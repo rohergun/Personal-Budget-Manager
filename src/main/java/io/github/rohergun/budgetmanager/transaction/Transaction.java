6f524fb48e93ddc93230adf802c0639489fcb15e
RohatErgun <rohatergun06@gmail.com>
2026-08-01 10:04:58 +0200

    feat: add Transcation class jpa relationships

--- src/main/java/io/github/rohergun/budgetmanager/transaction/Transaction.java
+++ src/main/java/io/github/rohergun/budgetmanager/transaction/Transaction.java
@@ -1,11 +1,15 @@
 package io.github.rohergun.budgetmanager.transaction;
 
+import io.github.rohergun.budgetmanager.category.Category;
 import io.github.rohergun.budgetmanager.model.BaseEntity;
+import io.github.rohergun.budgetmanager.user.AppUser;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.math.BigDecimal;
 
@@ -21,4 +25,13 @@ public class Transaction extends BaseEntity {
     @Column(name = "transaction_type", nullable = false)
     @Enumerated(EnumType.STRING)
     private TransactionType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = true)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    private Category category;
 }
