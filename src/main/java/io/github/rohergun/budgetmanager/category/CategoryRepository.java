94f06d45b3c374229258df5c43e5db64524b00ad
RohatErgun <rohatergun06@gmail.com>
2026-08-03 09:14:44 +0200

    feat: add existsByUserAndName CategoryRepository method

--- src/main/java/io/github/rohergun/budgetmanager/category/CategoryRepository.java
+++ src/main/java/io/github/rohergun/budgetmanager/category/CategoryRepository.java
@@ -1,5 +1,6 @@
 package io.github.rohergun.budgetmanager.category;
 
+import io.github.rohergun.budgetmanager.user.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    boolean existsByUserAndName(AppUser user, String name);
 }
