8574b87e4654babff251dbffedd752cf9b689131
RohatErgun <rohatergun06@gmail.com>
2026-08-04 17:42:13 +0200

    feat: add Mapping targets for flatting Category object

--- src/main/java/io/github/rohergun/budgetmanager/budget/BudgetMapper.java
+++ src/main/java/io/github/rohergun/budgetmanager/budget/BudgetMapper.java
@@ -2,8 +2,11 @@ package io.github.rohergun.budgetmanager.budget;
 
 import io.github.rohergun.budgetmanager.budget.dto.BudgetResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BudgetMapper {
+    @Mapping(target = "categoryId", source = "category.id")
+    @Mapping(target = "categoryName", source = "category.name")
     BudgetResponse toResponse(Budget budget);
 }
