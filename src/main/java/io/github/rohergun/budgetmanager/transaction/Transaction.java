2f0ab67bf49a41c8308db8c803f172bee52fe9b5
RohatErgun <rohatergun06@gmail.com>
2026-08-05 10:28:43 +0200

    feat: add TransactionDate field to Transaction.java

--- src/main/java/io/github/rohergun/budgetmanager/transaction/Transaction.java
+++ src/main/java/io/github/rohergun/budgetmanager/transaction/Transaction.java
@@ -4,19 +4,18 @@ import io.github.rohergun.budgetmanager.category.Category;
 import io.github.rohergun.budgetmanager.model.BaseEntity;
 import io.github.rohergun.budgetmanager.user.AppUser;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "transactions")
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
+@Builder
 public class Transaction extends BaseEntity {
 
     @Column(nullable = false)
@@ -34,4 +33,7 @@ public class Transaction extends BaseEntity {
     @JoinColumn(name = "category_id", nullable = true)
     @OnDelete(action = OnDeleteAction.SET_NULL)
     private Category category;
+
+    @Column(nullable = false, name = "transaction_date")
+    private LocalDateTime transactionDate;
 }
---
+++ src/main/java/io/github/rohergun/budgetmanager/transaction/TransactionServiceImpl.java
@@ -0,0 +1,2 @@
+package io.github.rohergun.budgetmanager.transaction;public class TransactionServiceImpl {
+}
