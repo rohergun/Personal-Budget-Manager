99604bf8db7ec446c262170fcf41770be7d743ab
RohatErgun <rohatergun06@gmail.com>
2026-09-22 17:45:10 +0200

    fix(CategoryUpdateRequest): remove NotBlank annotation from the description field

--- src/main/java/io/github/rohergun/budgetmanager/category/dto/CategoryUpdateRequest.java
+++ src/main/java/io/github/rohergun/budgetmanager/category/dto/CategoryUpdateRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Size;
 public record CategoryUpdateRequest(
         @NotBlank @Size(min = 2, max = 20)
         String name,
-        @NotBlank @Size(min = 3, max = 100)
+        @Size(min = 3, max = 100)
         String description
 ) { }
